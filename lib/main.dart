15713dc2d8467f11da341bb9b211c5a7231c87cf
sgm17 <sergigm78@gmail.com>
2024-09-23 11:23:07 +0200

    android foreground and background images fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:story_saver_video_downloader/screens/history_screen/history_screen.dart';
-import 'package:story_saver_video_downloader/screens/home_screen/home_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
