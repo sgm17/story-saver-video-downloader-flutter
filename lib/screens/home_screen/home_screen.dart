31bbc65eadf21f676ab3b524584e5c5b29f16a44
sgm17 <sergigm78@gmail.com>
2024-09-23 20:14:22 +0200

    highlight overlay implemented

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:story_saver_video_downloader/models/navigation_state.dart';
 import 'package:story_saver_video_downloader/providers/highlighted_y_position_provider.dart';
+import 'package:story_saver_video_downloader/providers/highlights_provider.dart';
 import 'package:story_saver_video_downloader/providers/navigation_state_provider.dart';
 import 'package:story_saver_video_downloader/providers/scroll_y_provider.dart';
 import 'package:story_saver_video_downloader/providers/story_y_position_provider.dart';
@@ -26,8 +27,8 @@ class HomeScreen extends ConsumerWidget {
         NavigationState.profile => const InstagramOverlay(),
         NavigationState.post => const NodeDownload(),
       },
-      StoryOverlay(),
-      HighlightedOverlay(),
+      const StoryOverlay(),
+      const HighlightedOverlay(),
       // TODO: DELETE THIS
       Align(
           alignment: Alignment.bottomLeft,
@@ -70,13 +71,25 @@ class HighlightedOverlay extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final highlightedYPosition = ref.watch(highlightedYPositionProvider);
     final scrollY = ref.watch(scrollYProvider);
+    final highlights = ref.watch(highlightsProvider);
+
     return Transform.translate(
       offset: Offset(10, highlightedYPosition - scrollY),
-      child: Container(
-        height: 56,
-        width: 56,
-        decoration: BoxDecoration(
-            shape: BoxShape.circle, border: Border.all(color: Colors.red)),
+      child: ListView.separated(
+        itemCount: highlights.length,
+        separatorBuilder: (context, index) {
+          return const SizedBox(
+            width: 5,
+          );
+        },
+        itemBuilder: (context, index) {
+          return Container(
+            height: 56,
+            width: 56,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle, border: Border.all(color: Colors.red)),
+          );
+        },
       ),
     );
   }
